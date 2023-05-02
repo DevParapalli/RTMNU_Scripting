d63edaa1ef40ea5b1aacf9c6530afe292e66c66c
Devansh Parapalli <parapallidev@gmail.com>
2023-05-02 22:25:28 +0530

    mate, I has achieved komedy

---
+++ rtmnuresults/index.js
@@ -0,0 +1,24 @@
+fetch("https://rtmnuresults.org/Default.aspx", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-GB,en-IN;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cache-control": "no-cache",
+    "content-type": "application/x-www-form-urlencoded",
+    "pragma": "no-cache",
+    "sec-ch-ua": "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1"
+  },
+  "referrer": "https://rtmnuresults.org/Default.aspx",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "result=rbtnreg1&ddlselectfaculty=1&ddlselectexam=10492&txtrollno=886040&imgbtnviewmarksheet.x=75&imgbtnviewmarksheet.y=20&example_length=5",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
+
---
+++ rtmnuresults/main.py
@@ -0,0 +1,46 @@
+import pathlib
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support import expected_conditions as EC
+import json
+
+
+seats = range(oh man, i want gf)
+
+current_dir = pathlib.Path(__file__).parent.absolute()
+
+chrome_options = webdriver.ChromeOptions()
+prefs = {'download.default_directory' : r"C:\DevParapalli\Projects\RTMNU_Scripting\rtmnuresults\result_images"}
+chrome_options.add_experimental_option('prefs', prefs)
+
+
+with webdriver.Chrome(chrome_options=chrome_options) as driver:
+    driver.get('https://rtmnuresults.org/')
+    driver.maximize_window();
+    html = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
+    driver.execute_script("document.body.style.zoom='125 %'")
+    faculty_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'ddlselectfaculty')))
+    Select(faculty_select).select_by_value('1')
+    exam_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'ddlselectexam')))
+    # Select(exam_select).select_by_value('10492')
+    Select(exam_select).select_by_value('10557')
+    roll_no_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'txtrollno')))
+    roll_no_input.send_keys(str(seats[0]))
+    submit_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'imgbtnviewmarksheet')))
+    submit_button.click()
+    for seat in seats:
+        # go back
+        driver.execute_script("window.history.go(-1)")
+        roll_no_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'txtrollno')))
+        roll_no_input.clear()
+        roll_no_input.send_keys(str(seat))
+        submit_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'imgbtnviewmarksheet')))
+        submit_button.click()
+        # wait for the image to load
+        image_frame = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'ContentPlaceHolder1_ifrm')))
+        driver.switch_to.frame(image_frame)
+        image = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'img')))
+        image.screenshot(str(current_dir / 'result_images' / f'{seat}.png'))
+        driver.switch_to.default_content()
